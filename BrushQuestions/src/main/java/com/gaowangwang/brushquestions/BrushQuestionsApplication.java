af6eae2ee470e40e80a9e5768bfcc50bacc1c6e0
gaojiawang <gaojiawang@gdcattsoft.com>
2022-03-14 17:42:24 +0800

    初始版本

---
+++ BrushQuestions/src/main/java/com/gaowangwang/brushquestions/BrushQuestionsApplication.java
@@ -0,0 +1,13 @@
+package com.gaowangwang.brushquestions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrushQuestionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrushQuestionsApplication.class, args);
+    }
+
+}
