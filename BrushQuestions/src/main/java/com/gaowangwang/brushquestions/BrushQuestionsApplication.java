270b69399df79336c128d8503ff4a76a78f422c9
gaojiawang <gaojiawang@gdcattsoft.com>
2022-03-15 16:39:45 +0800

    ignore

---
+++ BrushQuestions/src/main/java/com/gaowangwang/brushquestions/BrushQuestionsApplication.java
@@ -0,0 +1,16 @@
+package com.gaowangwang.brushquestions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 无敌小南瓜
+ */
+@SpringBootApplication
+public class BrushQuestionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrushQuestionsApplication.class, args);
+    }
+
+}
